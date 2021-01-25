0f801c4318a07244f6150cd0bf9069790e99e998
Kiwi <hallerlucas@outlook.com>
2021-01-25 21:59:39 +0100

    Create Checker.cs and add proxies list

--- NitroGen/Checker.cs
+++ NitroGen/Checker.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using Leaf.xNet;
+using Console = Colorful.Console;
 
 namespace NitroGen
 {
     class Checker
     {
+        static void Connect()
+        {
+            using (var request = new HttpRequest())
+            {
+                // Randomize User-agent for better anonymity
+                request.UserAgent = Http.RandomUserAgent();
+            }
+        }
     }
 }
